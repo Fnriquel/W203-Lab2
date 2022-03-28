984b4c30aed97ce3df1092ff96a5ccf0fa44dbba
Tianyu Wang <twang0@berkeley.edu>
2022-03-28 01:39:20 -0700

    Add one test file

--- Test.R
+++ Test.R
@@ -4,30 +4,18 @@ library(ggplot2)
 library(patchwork)
 library(sandwich)
 library(lmtest)
+library(ggpmisc)
+
 
 data_set <-read.csv("merged_data_cleaned.csv")
-data_set2<- data_set %>% filter(altitude_mean_meters<6000) %>% filter(Total.Cup.Points >0)
+data_set2<- data_set %>% filter(altitude_mean_meters<6000)
 
-lm1 <- lm(data_set2$Total.Cup.Points ~ altitude_mean_meters, data = data_set2)
-
-plot1 <- data_set2 %>%
+plot1 <- data_set %>%
+filter(altitude_mean_meters<6000 & altitude_mean_meters>500) %>%
   ggplot()+
   aes(x=altitude_mean_meters, y=Total.Cup.Points)+
   geom_jitter()+
-  geom_smooth(method = "lm", se = FALSE)+
-  annotate("text", x=3500, y=65, label= bquote(y==~"0.000823x + 81.09, "~R^2==~"0.023") )
+  geom_smooth(method = "lm", se = FALSE)
 plot1
 
-
-
-plot2 <- data_set2 %>%
-  ggplot()+
-  aes(altitude_mean_meters)+
-  geom_density()
-plot2
-
-plot3 <- data_set2 %>%
-  ggplot()+
-  aes(Total.Cup.Points)+
-  geom_density()
-plot3
+lm1 <- lm(data_set2$Total.Cup.Points ~ data_set2$)
